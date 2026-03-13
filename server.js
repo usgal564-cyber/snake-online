13dde2c49948172c80482c37b289c4dd49fd91cc
usgal564-cyber <usgal564@gmail.com>
2026-03-13 22:12:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,26 +5,49 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-io.on('connection', (socket) => {
-    console.log('Хэрэглэгч холбогдлоо');
+let rooms = {};
 
+io.on('connection', (socket) => {
     socket.on('createRoom', (data) => {
         const roomId = Math.random().toString(36).substring(2, 7).toUpperCase();
+        rooms[roomId] = { players: {}, food: {x: 15, y: 15, color: '#ff3e3e'} };
         socket.join(roomId);
         socket.emit('roomCreated', roomId);
     });
 
     socket.on('joinRoom', (data) => {
-        socket.join(data.code);
-        io.to(data.code).emit('joinedSuccess', data.code);
+        if (rooms[data.code]) {
+            socket.join(data.code);
+            socket.emit('joinedSuccess', data.code);
+        } else {
+            socket.emit('error', 'Өрөө олдсонгүй!');
+        }
     });
 
-    socket.on('disconnect', () => {
-        console.log('Хэрэглэгч саллаа');
+    socket.on('updatePos', (data) => {
+        if (rooms[data.code]) {
+            rooms[data.code].players[socket.id] = {
+                snake: data.snake,
+                score: data.score,
+                name: data.name,
+                color: data.color
+            };
+            io.to(data.code).emit('gameState', rooms[data.code]);
+        }
+    });
+
+    socket.on('foodEaten', (data) => {
+        if (rooms[data.code]) {
+            const colors = ['#FFD700', '#FF00FF', '#00FFFF', '#ADFF2F', '#FF4500'];
+            rooms[data.code].food = {
+                x: Math.floor(Math.random() * 25),
+                y: Math.floor(Math.random() * 25),
+                color: colors[Math.floor(Math.random() * colors.length)]
+            };
+            io.to(data.code).emit('gameState', rooms[data.code]);
+        }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Сервер ${PORT} дээр ажиллаж байна`);
-});
+http.listen(PORT, () => console.log(`Server running on ${PORT}`));
