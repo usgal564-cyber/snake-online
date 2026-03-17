01c09a2c156b5ffe97bb59da0f087f5eef0945e1
usgal564-cyber <usgal564@gmail.com>
2026-03-17 13:58:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,57 +6,52 @@ const io = require('socket.io')(http, { cors: { origin: "*" } });
 app.use(express.static(__dirname));
 
 let rooms = {};
-const textBank = [
-    "the quick brown fox jumps over the lazy dog",
-    "programming is the art of telling a computer what to do",
-    "javascript is a versatile language for web development",
-    "practice makes perfect when it comes to typing speed"
-];
+const words = ["the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog", "programming", "javascript", "code", "typing", "speed", "challenge", "monkey", "keyboard", "battle", "online", "developer", "system", "logic", "word", "test", "practice", "future", "world", "simple", "complex", "game", "winner", "screen", "focus", "rhythm", "accuracy", "percent", "minute", "stable", "server", "connection", "dynamic", "static", "render", "github", "friend", "room", "start", "finish", "fast", "slow", "energy"];
+
+function generateText(len) {
+    let result = [];
+    for(let i=0; i<len; i++) result.push(words[Math.floor(Math.random() * words.length)]);
+    return result.join(" ");
+}
 
 io.on('connection', (socket) => {
     socket.on('createRoom', (data) => {
         const roomId = Math.random().toString(36).substring(2, 7).toUpperCase();
         rooms[roomId] = { 
             players: {}, 
-            text: textBank[Math.floor(Math.random() * textBank.length)],
-            status: 'waiting'
+            text: generateText(50), // 50 үг
+            status: 'waiting',
+            host: socket.id
         };
         socket.join(roomId);
-        socket.emit('roomCreated', roomId);
+        socket.emit('roomCreated', { code: roomId, isHost: true });
     });
 
     socket.on('joinRoom', (data) => {
-        const roomCode = data.code.toUpperCase();
-        if (rooms[roomCode]) {
-            socket.join(roomCode);
-            socket.emit('joinedSuccess', {code: roomCode, text: rooms[roomCode].text});
+        const code = data.code.toUpperCase();
+        if (rooms[code]) {
+            socket.join(code);
+            socket.emit('joinedSuccess', { code: code, isHost: false, text: rooms[code].text });
         } else {
             socket.emit('errorMsg', 'Өрөө олдсонгүй!');
         }
     });
 
-    socket.on('playerReady', (data) => {
-        const room = rooms[data.code.toUpperCase()];
-        if (room) {
-            room.players[socket.id] = { name: data.name, progress: 0, wpm: 0, ready: true };
-            const playersArr = Object.values(room.players);
-            if (playersArr.length >= 2) {
-                room.status = 'playing';
-                io.to(data.code.toUpperCase()).emit('gameStart');
-            }
+    socket.on('startGame', (data) => {
+        if (rooms[data.code] && rooms[data.code].host === socket.id) {
+            rooms[data.code].status = 'playing';
+            io.to(data.code).emit('gameStart');
         }
     });
 
     socket.on('updateProgress', (data) => {
-        const room = rooms[data.code.toUpperCase()];
-        if (room && room.players[socket.id]) {
-            room.players[socket.id].progress = data.progress;
-            room.players[socket.id].wpm = data.wpm;
-            io.to(data.code.toUpperCase()).emit('gameState', room.players);
-            
-            if (data.progress >= 100) {
-                io.to(data.code.toUpperCase()).emit('gameOver', room.players[socket.id].name);
+        const room = rooms[data.code];
+        if (room) {
+            room.players[socket.id] = { name: data.name, progress: data.progress, wpm: data.wpm };
+            io.to(data.code).emit('gameState', room.players);
+            if (data.progress >= 100 && room.status !== 'ended') {
                 room.status = 'ended';
+                io.to(data.code).emit('gameOver', data.name);
             }
         }
     });
